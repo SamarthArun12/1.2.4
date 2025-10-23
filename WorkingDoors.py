fc1fffccd3572d590f297e78983789889f8d586d
SamarthArun12 <samarth.arun.ca@gmail.com>
2025-10-23 09:25:41 -0700

    Add files via upload

---
+++ WorkingDoors.py
@@ -0,0 +1,22 @@
+import turtle as trtl
+
+wn = trtl.Screen()
+
+maze_draw = trtl.Turtle()
+dist = 20
+steps = 2
+maze_draw.speed(0)
+maze_draw.right(270)
+maze_draw.forward(dist)
+while steps < 31:
+    for i in range(1, steps):
+        if i % 11 == 0:
+            maze_draw.penup()
+        maze_draw.forward(dist)
+        maze_draw.pendown()
+    maze_draw.right(270)
+    steps += 1
+
+maze_draw.hideturtle()
+
+wn.mainloop()
